db0027b24313dbce4d942241afe0115a90ae506c
Александр Смолин <smolll123@mail.ru>
2022-01-16 14:15:21 +0300

    added HEADERS constant to further pass it as a parameter to requests.get()

--- parser_test.py
+++ parser_test.py
@@ -1,2 +1,10 @@
 import requests
 from bs4 import BeautifulSoup as BS
+
+
+HEADERS = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/96.0.4664.45 Safari/537.36',
+    'accept': '*/*'
+}
